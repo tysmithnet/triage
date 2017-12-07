06f5b454c61d6261e04299798322c080d5e2282a
Ty Smith <dts5004@gmail.com>
2017-12-06 23:34:39 -0800

    Added command line parsing functionality

--- Triage/Triage.Mortician/Program.cs
+++ Triage/Triage.Mortician/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommandLine;
+using Microsoft.Diagnostics.Runtime;
 
 namespace Triage.Mortician
 {
@@ -10,6 +12,24 @@ namespace Triage.Mortician
     {
         static void Main(string[] args)
         {
+            Parser.Default.ParseArguments<CommandLineOptions>(args).WithParsed(options =>
+            {
+                Console.WriteLine("hello");
+            });
+        }      
+    }
+
+    public class CommandLineOptions
+    {
+        [Option('d', "dumpfile", HelpText = "Full path to the dump file", Required = true)]
+        public string DumpFilePath { get; set; }
+
+        [Option('n', "name", HelpText = "User provided name for this report", Required = false)]
+        public string ReportName { get; set; }
+
+        public CommandLineOptions()
+        {
+            ReportName = DateTime.Now.ToString("yyyyMMdd_hhmmss-mortician");
         }
     }
 }
