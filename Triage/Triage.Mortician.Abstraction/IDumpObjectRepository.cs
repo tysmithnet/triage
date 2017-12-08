77efff8b2d355a189bf26e0ffccd85ae2357a09c
Ty Smith <dts5004@gmail.com>
2017-12-08 00:26:32 -0800

    Non functional refactoring

---
+++ Triage/Triage.Mortician.Abstraction/IDumpObjectRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Triage.Mortician.Abstraction
+{
+    public interface IDumpObjectRepository
+    {
+        IDumpObject Get(ulong address);           
+    }
+}
--- Triage/Triage.Mortician.Abstraction/IPlugin.cs
+++ Triage/Triage.Mortician.Abstraction/IPlugin.cs
@@ -4,23 +4,11 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Diagnostics.Runtime;
 
 namespace Triage.Mortician.Abstraction
 {
     public interface IPlugin
     {
         Task Setup();
-    }
-
-    public interface IObjectInspector
-    {
-        bool CanInspect(ClrObject clrObject, ClrRuntime runtime, DataTarget dataTarget);
-        IDumpObject Insepct(ClrObject clrObject, ClrRuntime runtime, DataTarget dataTarget);
-    }
-
-    public interface IDumpObjectRepository
-    {
-        IDumpObject Get(ulong address);           
     }
 }
