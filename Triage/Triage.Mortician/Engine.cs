c9382299d61d17adb3adc9751c22045c0db0fd36
Ty Smith <dts5004@gmail.com>
2017-12-08 01:46:51 -0800

    Added initial engine logic

--- Triage/Triage.Mortician/Engine.cs
+++ Triage/Triage.Mortician/Engine.cs
@@ -1,11 +1,67 @@
-﻿using System.ComponentModel.Composition;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Common.Logging;
 using Triage.Mortician.Abstraction;
 
 namespace Triage.Mortician
 {
+    [Export]
     public class Engine
     {
         [ImportMany]
         public IAnalyzer[] Analyzers { get; set; }
+
+        protected internal ILog Log { get; set; } = LogManager.GetLogger(typeof(Engine));
+
+        public Task Process(CancellationToken cancellationToken)
+        {
+            if (Analyzers == null || Analyzers.Length == 0)
+            {
+                Log.Fatal("No analyzers were found!");
+                return Task.FromResult(0);
+            }
+            
+            Log.Trace("Engine starting...");
+
+            var tasks = new List<Task>();
+
+            foreach (var analyzer in Analyzers)
+            {
+                var copy = analyzer;
+                var task = analyzer
+                    .Setup(cancellationToken)
+                    .ContinueWith(async t =>
+                    {
+                        if (t.IsCanceled)
+                        {
+                            await t;
+                        }
+
+                        if (t.IsFaulted)
+                        {
+                            Log.Error($"Analyzer: {copy.GetType()} failed", t.Exception);
+                            await t;
+                        }
+
+                        try
+                        {
+                            cancellationToken.ThrowIfCancellationRequested();
+                            await copy.Process(cancellationToken);
+                        }
+                        catch (Exception e)
+                        { 
+                            Log.Error($"Analyzer: {copy.GetType()} failed", e);
+                        }
+                    }, cancellationToken);
+
+                tasks.Add(task);
+            }
+
+            return Task.WhenAll(tasks);
+        }          
     }
 }
