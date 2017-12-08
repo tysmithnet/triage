3d1aba1cecd7249c5828573924f74157a9d470e0
Ty Smith <dts5004@gmail.com>
2017-12-08 01:52:18 -0800

    Added first analyzer stub

---
+++ Triage/Triage.Mortician.Analyzers/ThreadBuildUpAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Common.Logging;
+using Triage.Mortician.Abstraction;
+
+namespace Triage.Mortician.Analyzers
+{
+
+    public class ThreadBuildUpAnalyzer : IAnalyzer
+    {                                 
+        public ILog Log { get; set; } = LogManager.GetLogger(typeof(ThreadBuildUpAnalyzer));
+
+        public Task Setup(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task Process(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
