e6f624c1f5eea6076eae62f3a88e3e9752555732
Ty Smith <dts5004@gmail.com>
2017-12-09 13:30:32 -0800

    Added first layout stub using nebular

--- Triage/triage-ui/src/app/dash/dash.module.ts
+++ Triage/triage-ui/src/app/dash/dash.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NbSidebarModule, NbLayoutModule, NbSidebarService } from '@nebular/theme';
+
 import { DashRoutingModule } from './dash-routing.module';
 import { DashComponent } from './dash.component';
+
 
 @NgModule({
   imports: [
     CommonModule,
-    DashRoutingModule
+    DashRoutingModule,
+    NbLayoutModule,
+    NbSidebarModule
   ],
+  providers: [NbSidebarService],
   declarations: [DashComponent],
   exports: [DashComponent]
 })
