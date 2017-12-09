45445c12383e356803f6f80fd55348b3f80ef44d
Ty Smith <dts5004@gmail.com>
2017-12-09 01:20:57 -0800

    Migrated console application logic to MVC

---
+++ Triage/Triage.Mortician.Web/DumpObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Triage.Mortician.Abstraction;
+
+namespace Triage.Mortician.Web
+{
+    [DebuggerDisplay("{FullTypeName} : {Size} : {Address}")]
+    public class DumpObject : IDumpObject
+    {
+        public ulong Address { get; internal set; }
+        public string FullTypeName { get; internal set; }
+        public ulong Size { get; internal set; }
+
+        internal IList<IDumpObject> ReferencesInternal = new List<IDumpObject>();
+        public IReadOnlyCollection<IDumpObject> References { get; internal set; }
+
+        public void AddReference(IDumpObject obj)
+        {
+            ReferencesInternal.Add(obj);
+        }
+
+        public DumpObject(ulong address, string fullTypeName, ulong size)
+        {
+            Address = address;
+            FullTypeName = fullTypeName;
+            Size = size;
+        }                                                                      
+    }
+}
