0e3581eb7feb7c8064b69881d410f8ec53642861
Ty Smith <dts5004@gmail.com>
2017-12-11 00:19:24 -0800

    Created first excel output document

---
+++ Triage/Triage.Mortician.Analyzers/ExcelAnalyzerHost.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;     
+using Triage.Mortician.Abstraction;      
+using SpreadsheetLight;
+
+namespace Triage.Mortician.Analyzers
+{
+    [Export(typeof(IAnalyzer))]
+    public class ExcelAnalyzerHost : IAnalyzer
+    {
+        [ImportMany]
+        public IExcelAnalyzer[] ExcelAnalyzers { get; set; }
+
+        public Task Setup(CancellationToken cancellationToken)
+        {                         
+            var doc = new SLDocument();
+            foreach (var excelAnalyzer in ExcelAnalyzers)
+            {
+                excelAnalyzer.Contribute(doc);
+            }
+
+            doc.SaveAs("findme.xls");
+            return Task.CompletedTask;
+        }
+
+        public Task Process(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    public interface IExcelAnalyzer
+    {
+        void Contribute(SLDocument sharedDocument);
+    }
+
+    [Export(typeof(IExcelAnalyzer))]
+    public class HelloExcel : IExcelAnalyzer
+    {
+        public void Contribute(SLDocument sharedDocument)
+        {
+            sharedDocument.AddWorksheet("Hello world");
+        }
+    }
+}
