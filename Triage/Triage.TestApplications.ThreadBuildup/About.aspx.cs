693e75732440b9de344fb7356f2ce2c84637c0f9
Ty Smith <dts5004@gmail.com>
2017-12-16 15:09:18 -0800

    Made it so About page causes thread build up

--- Triage/Triage.TestApplications.ThreadBuildup/About.aspx.cs
+++ Triage/Triage.TestApplications.ThreadBuildup/About.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -11,7 +12,7 @@ namespace Triage.TestApplications.ThreadBuildup
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Thread.Sleep(1000000);
         }
-    }
+    }              
 }
