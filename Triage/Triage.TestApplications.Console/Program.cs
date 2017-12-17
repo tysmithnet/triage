874e1627eac12cb8a03b6fe3ea582bf92f3c0c98
Ty Smith <dts5004@gmail.com>
2017-12-16 22:34:59 -0800

    Added ThreadBuildup problem in Console test application

--- Triage/Triage.TestApplications.Console/Program.cs
+++ Triage/Triage.TestApplications.Console/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using static System.Console;
 
 namespace Triage.TestApplications.Console
 {
@@ -10,7 +8,49 @@ namespace Triage.TestApplications.Console
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello world");
+            WriteLine("Which scenario do you want to run?");
+            WriteLine("1) Thread Buildup");
+            do
+            {
+                Write("Selection: ");
+                var isConverted = Int32.TryParse(ReadLine(), out var selection);
+                if (!isConverted)
+                {
+                    WriteLine("Please enter a number");
+                    continue;
+                }
+                switch (selection)
+                {
+                    case 1:
+                        ThreadBuildup();
+                        Thread.Sleep(1000);
+                        WriteLine("Ok to take dump now");
+                        ReadLine();
+                        return;
+                    default:
+                        continue;
+                }
+                
+            } while (true);
+        }
+
+        private static void ThreadBuildup()
+        {
+            const int NUM_THREADS = 10;
+            for (int i = 0; i < NUM_THREADS; i++)
+            {                                                  
+                var thread = new Thread(DoNothing);
+                thread.IsBackground = true;
+                thread.Start();
+            }
+        }
+
+        private static void DoNothing()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                Thread.Sleep(1000);
+            }
         }
     }
 }
