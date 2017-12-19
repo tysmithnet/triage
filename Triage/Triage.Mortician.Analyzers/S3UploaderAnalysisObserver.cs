117a3d83f677a407d17b29dc347ec95834f90063
Ty Smith <dts5004@gmail.com>
2017-12-18 21:40:51 -0800

    Trying something with the put/get S3 functions

--- Triage/Triage.Mortician.Analyzers/S3UploaderAnalysisObserver.cs
+++ Triage/Triage.Mortician.Analyzers/S3UploaderAnalysisObserver.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.Composition;
+﻿using System;
+using System.ComponentModel.Composition;
 using System.IO;
 using System.Net;
 using System.Reactive.Linq;
@@ -62,15 +63,15 @@ namespace Triage.Mortician.Analyzers
         /// <returns>A task that when complete will signal the completion of this work</returns>
         public Task Process(CancellationToken cancellationToken)
         {
-            return EventHub.Get<ExcelReportComplete>().ForEachAsync(async message =>
+            return EventHub.Get<ExcelReportComplete>().ForEachAsync(message =>
             {
-                var shouldUpload = SettingsRepository.GetBool(UploadExcelToS3, fallbackToDefault:true);
+                var shouldUpload = SettingsRepository.GetBool(UploadExcelToS3, fallbackToDefault: true);
                 if (!shouldUpload)
                 {
                     Log.Trace($"Skipping upload excel report to s3. Is the setting {UploadExcelToS3} set to true?");
                     return;
                 }
-                               
+
                 // note: this looks for ~/.aws/credentials for a profile named default
                 // see https://docs.aws.amazon.com/AmazonS3/latest/dev/walkthrough1.html#walkthrough1-add-users
                 // todo: move this out to a decoupled component
@@ -89,13 +90,18 @@ namespace Triage.Mortician.Analyzers
 
                     // todo: make a service
                     Log.Info("Attempting to upload report to S3");
-                    var putObjectResponse = await client.PutObjectAsync(putReportRequest, cancellationToken);
-
-                    if (putObjectResponse.HttpStatusCode == HttpStatusCode.OK)
-                        Log.Trace($"Upload of {message.ReportFile} was successful");
-                    else
-                        Log.Error($"Unable to upload {message.ReportFile} to S3");
-                    
+                    try
+                    {
+                        var putObjectResponse = client.PutObject(putReportRequest);
+                        if (putObjectResponse.HttpStatusCode == HttpStatusCode.OK)
+                            Log.Trace($"Upload of {message.ReportFile} was successful");
+                        else
+                            throw new ApplicationException("Unknown error");
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error($"Unable to upload {message.ReportFile} to S3: {e.Message}", e);
+                    }
                 }
             }, cancellationToken);
         }
