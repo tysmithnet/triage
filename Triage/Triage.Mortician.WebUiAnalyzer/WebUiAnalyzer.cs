5d3df1b20ceeb7011ab7151a19f543da2e47624b
Ty Smith <dts5004@gmail.com>
2018-01-20 22:59:49 -0800

    Added new project for WebUi Analyzer

---
+++ Triage/Triage.Mortician.WebUiAnalyzer/WebUiAnalyzer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Triage.Mortician.WebUiAnalyzer
+{
+    public class WebUiAnalyzer : IAnalyzer
+    {
+        public Task Setup(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Process(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
