cb559aabaff0227352ac4ed58ab682bde0b72dc8
Ty Smith <dts5004@gmail.com>
2018-09-18 00:57:15 -0700

    Created first test for the engine

--- Triage/Triage.Mortician.Test/Engine_Should.cs
+++ Triage/Triage.Mortician.Test/Engine_Should.cs
@@ -12,22 +12,28 @@ namespace Triage.Mortician.Test
     public class Engine_Should
     {
         [Fact]
-        public void Shutdown_The_Event_Hub_When_Analyzers_Have_Finished()
+        public async Task Shutdown_The_Event_Hub_When_Analyzers_Have_Finished()
         {
             // arrange
             var eventHubMock = new Mock<IEventHub>();
-            var analyzerMock = new Mock<IAnalyzer>();
-            
+            var taskFactoryMock = new Mock<IAnalyzerTaskFactory>();
+            taskFactoryMock
+                .Setup(factory =>
+                    factory.StartAnalyzers(It.IsAny<IEnumerable<IAnalyzer>>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.CompletedTask);
+
             var engine = new Engine()
             {
-                Analyzers = new [] {analyzerMock.Object},
-                EventHub = eventHubMock.Object
+                Analyzers = new [] {new Mock<IAnalyzer>().Object},
+                EventHub = eventHubMock.Object,
+                AnalyzerTaskFactory = taskFactoryMock.Object
             };
 
             // act
-
+            await engine.Process();
 
             // assert
+            eventHubMock.Verify(hub => hub.Shutdown(), Times.Once);
         }
     }
 }
