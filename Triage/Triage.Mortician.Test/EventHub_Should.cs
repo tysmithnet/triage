a86e49c192ee9c3adc6fcb84e7c01035dcf81ac7
Ty Smith <dts5004@gmail.com>
2018-09-18 01:38:21 -0700

    Added place holder for event hub tests

---
+++ Triage/Triage.Mortician.Test/EventHub_Should.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triage.Mortician.Test
+{
+    public class EventHub_Should
+    {
+        public void Get_All_Types_At_Or_Bellow_The_Requested()
+        {
+            // arrange
+            
+            // act
+
+            // assert
+        }
+    }
+}
