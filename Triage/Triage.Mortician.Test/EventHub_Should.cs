521911bc7b2568651fa7c0f9ed41e8b68d6e6f96
Ty Smith <dts5004@gmail.com>
2018-09-18 01:51:11 -0700

    Added first test for EventHub

--- Triage/Triage.Mortician.Test/EventHub_Should.cs
+++ Triage/Triage.Mortician.Test/EventHub_Should.cs
@@ -1,20 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Reactive.Linq;
 using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
 
 namespace Triage.Mortician.Test
 {
     public class EventHub_Should
     {
-        public void Get_All_Types_At_Or_Bellow_The_Requested()
+        private class A : Message
+        {
+        }
+
+        private class B : A
+        {
+        }
+
+        private class C : B
+        {
+        }
+
+        [Fact]
+        public async Task Get_All_Types_At_Or_Bellow_The_Requested()
         {
             // arrange
-            
+            var eventHub = new EventHub();
+            eventHub.Broadcast(new A());
+            eventHub.Broadcast(new B());
+            eventHub.Broadcast(new C());
+            eventHub.Shutdown();
+
             // act
+            var isGood = await eventHub.Get<B>().All(b => b is B);
 
             // assert
+            isGood.Should().BeTrue();
         }
     }
 }
