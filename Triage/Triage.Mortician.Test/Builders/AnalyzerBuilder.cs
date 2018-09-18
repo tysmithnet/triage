a07566da94af97bc447ac0a7eb25602e1417a121
Ty Smith <dts5004@gmail.com>
2018-09-18 00:34:41 -0700

    Added more pieces required to start testing Engine

---
+++ Triage/Triage.Mortician.Test/Builders/AnalyzerBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+
+namespace Triage.Mortician.Test.Builders
+{
+    public class AnalyzerBuilder
+    {
+        private Mock<IAnalyzer> _mock = new Mock<IAnalyzer>();
+        
+        public IAnalyzer Build()
+        {
+            return _mock.Object;
+        }
+    }
+}
---
+++ Triage/Triage.Mortician.Test/Engine_Should.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+
+namespace Triage.Mortician.Test
+{
+    public class Engine_Should
+    {
+        [Fact]
+        public void Shutdown_The_Event_Hub_When_Analyzers_Have_Finished()
+        {
+            // arrange
+            var eventHubMock = new Mock<IEventHub>();
+            var analyzerMock = new Mock<IAnalyzer>();
+            
+            var engine = new Engine()
+            {
+                Analyzers = new [] {analyzerMock.Object},
+                EventHub = eventHubMock.Object
+            };
+
+            // act
+
+
+            // assert
+        }
+    }
+}
