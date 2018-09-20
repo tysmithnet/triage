989c57e08ae7148d3859f21f80d529a395c70087
Ty Smith <dts5004@gmail.com>
2018-09-19 18:40:38 -0700

    Added start of new settings scheme

---
+++ Triage/Triage.Mortician/EngineSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Triage.Mortician.Core;
+
+namespace Triage.Mortician
+{
+    [Export(typeof(ISettings))]
+    [Export(typeof(EngineSettings))]
+    public class EngineSettings : ISettings
+    {
+        public string TestString { get; set; }
+    }
+}
