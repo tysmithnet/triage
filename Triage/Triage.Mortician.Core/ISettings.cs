c630370b296fb2c8dc3f1249c846b78624f5bb06
Ty Smith <dts5004@gmail.com>
2018-09-20 19:45:51 -0700

    Added more autogenerated comments

--- Triage/Triage.Mortician.Core/ISettings.cs
+++ Triage/Triage.Mortician.Core/ISettings.cs
@@ -1,5 +1,22 @@
-﻿namespace Triage.Mortician.Core
+﻿// ***********************************************************************
+// Assembly         : Triage.Mortician.Core
+// Author           : @tysmithnet
+// Created          : 09-19-2018
+//
+// Last Modified By : @tysmithnet
+// Last Modified On : 09-20-2018
+// ***********************************************************************
+// <copyright file="ISettings.cs" company="">
+//     Copyright ©  2018
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+namespace Triage.Mortician.Core
 {
+    /// <summary>
+    ///     Interface ISettings
+    /// </summary>
     public interface ISettings
     {
     }
--- Triage/Triage.Mortician/Adapters/BaseAdapter.cs
+++ Triage/Triage.Mortician/Adapters/BaseAdapter.cs
@@ -1,16 +1,46 @@
-﻿using System;
+﻿// ***********************************************************************
+// Assembly         : Triage.Mortician
+// Author           : @tysmithnet
+// Created          : 09-20-2018
+//
+// Last Modified By : @tysmithnet
+// Last Modified On : 09-20-2018
+// ***********************************************************************
+// <copyright file="BaseAdapter.cs" company="">
+//     Copyright ©  2017
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+using System;
 
 namespace Triage.Mortician.Adapters
 {
+    /// <summary>
+    ///     Class BaseAdapter.
+    /// </summary>
     internal abstract class BaseAdapter
     {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="BaseAdapter" /> class.
+        /// </summary>
+        /// <param name="converter">The converter.</param>
+        /// <exception cref="ArgumentNullException">converter</exception>
         /// <inheritdoc />
         protected BaseAdapter(IConverter converter)
         {
             Converter = converter ?? throw new ArgumentNullException(nameof(converter));
         }
 
+        /// <summary>
+        ///     Setups this instance.
+        /// </summary>
         public abstract void Setup();
+
+        /// <summary>
+        ///     Gets or sets the converter.
+        /// </summary>
+        /// <value>The converter.</value>
         internal IConverter Converter { get; set; }
     }
 }
