be4818e9300649ffe404b8367d49c0404a772028
Ty Smith <dts5004@gmail.com>
2018-09-24 22:34:55 -0700

    Added DumpTypeKey tests

---
+++ Triage/Triage.Mortician.Core.Test/DumpTypeKey_Should.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Triage.Mortician.Core.Test
+{
+    public class DumpTypeKey_Should
+    {
+        [Fact]
+        public void Disallow_Method_Table_Value_Of_Zero()
+        {
+            // arrange
+            Action throws = () => new DumpTypeKey(0, "ThisDoesntMatter");
+            
+            // act
+            // assert
+            throws.Should().Throw<ArgumentOutOfRangeException>();
+        }
+    }
+}
