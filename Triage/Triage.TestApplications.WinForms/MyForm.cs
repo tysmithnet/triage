e6bafe2bfd7eaae98a5b5b59ed7104dfb257b1ab
Ty Smith <dts5004@gmail.com>
2018-09-25 01:25:08 -0700

    Added a win forms test application

---
+++ Triage/Triage.TestApplications.WinForms/MyForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+using System.Windows.Forms;
+using Triage.Mortician.IntegrationTest;
+using static System.IO.Path;
+
+namespace Triage.TestApplications.WinForms
+{
+    public partial class MyForm : Form
+    {
+        public MyForm()
+        {
+            InitializeComponent();
+            var button = new Button
+            {
+                Text = "Hello World"
+            };
+            button.Click += (sender, args) => button.Text = "Clicked!";
+            Controls.Add(button);
+            DumpHelper.CreateDump(Combine(
+                ConfigurationManager.AppSettings[IntPtr.Size == 4 ? "DumpLocationX86" : "DumpLocationX64"],
+                "helloworld.dmp"));
+        }
+    }
+}
