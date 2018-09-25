35d353961971e560b9e8f1dc75943b0919083d63
Ty Smith <dts5004@gmail.com>
2018-09-24 23:28:56 -0700

    Added VirtualQueryData test

---
+++ Triage/Triage.Mortician.Core.Test/VirtualQueryData_Should.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Triage.Mortician.Core.ClrMdAbstractions;
+using Xunit;
+
+namespace Triage.Mortician.Core.Test
+{
+    public class VirtualQueryData_Should
+    {
+        [Fact]
+        public void Require_Create_An_Instance_With_Required_Parameters()
+        {
+            // arrange
+            var sut = new VirtualQueryData(0x1337, 0x42);
+
+            // act
+            // assert
+            sut.BaseAddress.Should().Be(0x1337);
+            sut.Size.Should().Be(0x42);
+        }
+    }
+}
