3c0455f6f99604d7e4a54f1ccc56e1e06d09e684
Ty Smith <dts5004@gmail.com>
2018-09-26 14:15:49 -0700

    Added !runaway processing support

--- Triage/Triage.Mortician.Test/RunawayOutputProcessor_Should.cs
+++ Triage/Triage.Mortician.Test/RunawayOutputProcessor_Should.cs
@@ -23,7 +23,8 @@ namespace Triage.Mortician.Test
             // assert
             report.RunawayLines.Should().HaveCount(7);
             report.RunawayLines.First().UserModeTime.Should().Be(TimeSpan.FromMilliseconds(15));
-            report.RunawayLines.First().KernelModeTime.Should().Be(TimeSpan.FromMilliseconds(46));
+            report.RunawayLines.First().KernelModeTime.Should().Be(TimeSpan.FromDays(1).Add(TimeSpan.FromMilliseconds(46)));
+            report.RunawayLines.First().TotalTime.Should().Be(TimeSpan.FromDays(1).Add(TimeSpan.FromMilliseconds(61)));
         }
 
         private const string BASIC0 = @" User Mode Time
@@ -37,7 +38,7 @@ namespace Triage.Mortician.Test
    1:6460      0 days 0:00:00.000
  Kernel Mode Time
   Thread       Time
-   0:2024      0 days 0:00:00.046
+   0:2024      1 days 0:00:00.046
    6:375c      0 days 0:00:00.000
    5:68a0      0 days 0:00:00.000
    4:24e0      0 days 0:00:00.000
