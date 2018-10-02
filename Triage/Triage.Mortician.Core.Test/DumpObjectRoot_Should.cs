2255d0500f3d7210936a091bfcef0b9a70358015
Ty Smith <dts5004@gmail.com>
2018-10-02 00:57:56 -0700

    Added coverage for DumpObjectRoot

--- Triage/Triage.Mortician.Core.Test/DumpObjectRoot_Should.cs
+++ Triage/Triage.Mortician.Core.Test/DumpObjectRoot_Should.cs
@@ -1,6 +1,5 @@
 using System;
 using FluentAssertions;
-using Triage.Mortician.Core.ClrMdAbstractions;
 using Triage.Testing.Common;
 using Xunit;
 
@@ -8,6 +7,70 @@ namespace Triage.Mortician.Core.Test
 {
     public class DumpObjectRoot_Should : BaseTest
     {
+        [Fact]
+        public void Add_A_Thread_Only_Once()
+        {
+            // arrange
+            var sut = new DumpObjectRoot
+            {
+                Address = 0x4000
+            };
+            var thread = new DumpThread
+            {
+                OsId = 0x40
+            };
+
+            // act
+            sut.AddThread(thread);
+            sut.AddThread(thread);
+
+            // assert
+            sut.Threads.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void Exhibit_Entity_Equality()
+        {
+            var a = new DumpObjectRoot
+            {
+                Address = 0,
+                Name = "Something"
+            };
+            var b = new DumpObjectRoot
+            {
+                Address = 0,
+                Name = "Something Else"
+            };
+            var c = new DumpObjectRoot
+            {
+                Address = 1,
+                Name = "Something"
+            };
+
+            a.GetHashCode().Should().Be(b.GetHashCode());
+            a.GetHashCode().Should().NotBe(c.GetHashCode());
+
+            a.Equals(a).Should().BeTrue();
+            a.Equals(b).Should().BeTrue();
+            a.Equals(c).Should().BeFalse();
+            a.Equals(null).Should().BeFalse();
+            a.Equals("").Should().BeFalse();
+            a.CompareTo(a).Should().Be(0);
+            a.CompareTo(b).Should().Be(0);
+            a.CompareTo(c).Should().Be(-1);
+            a.CompareTo(null).Should().Be(1);
+            a.Equals((object) a).Should().BeTrue();
+            a.Equals((object) b).Should().BeTrue();
+            a.Equals((object) c).Should().BeFalse();
+            a.Equals((object) null).Should().BeFalse();
+            a.CompareTo((object) a).Should().Be(0);
+            a.CompareTo((object) b).Should().Be(0);
+            a.CompareTo((object) c).Should().Be(-1);
+            a.CompareTo((object) null).Should().Be(1);
+            Action throws = () => a.CompareTo("");
+            throws.Should().Throw<ArgumentException>();
+        }
+
         [Fact]
         public void Should_Construct_Without_Error()
         {
@@ -17,6 +80,6 @@ namespace Triage.Mortician.Core.Test
             // act
             // assert
             sut.Should().NotThrow();
-		}
+        }
     }
 }
