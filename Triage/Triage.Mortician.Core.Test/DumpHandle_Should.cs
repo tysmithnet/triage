67499b9c796e6952af463777290f682f6dd6ae9e
Ty Smith <dts5004@gmail.com>
2018-10-02 19:17:50 -0700

    Added 100% code coverage of Core

--- Triage/Triage.Mortician.Core.Test/DumpHandle_Should.cs
+++ Triage/Triage.Mortician.Core.Test/DumpHandle_Should.cs
@@ -18,5 +18,52 @@ namespace Triage.Mortician.Core.Test
             // assert
             sut.Should().NotThrow();
 		}
+
+        [Fact]
+        public void Exhibit_Entity_Equality()
+        {
+            // arrange
+            var a = new DumpHandle()
+            {
+                Address = 0
+            };
+            var b = new DumpHandle()
+            {
+                Address = 0
+            };
+            var c = new DumpHandle()
+            {
+                Address = 1
+            };
+
+            // act
+            // assert
+            a.GetHashCode().Should().Be(b.GetHashCode());
+            a.GetHashCode().Should().NotBe(c.GetHashCode());
+
+            a.Equals(a).Should().BeTrue();
+            a.Equals(b).Should().BeTrue();
+            a.Equals(c).Should().BeFalse();
+            a.Equals(null).Should().BeFalse();
+            a.Equals("").Should().BeFalse();
+            a.CompareTo(a).Should().Be(0);
+            a.CompareTo(b).Should().Be(0);
+            a.CompareTo(c).Should().Be(-1);
+            a.CompareTo(null).Should().Be(1);
+            a.Equals((object)a).Should().BeTrue();
+            a.Equals((object)b).Should().BeTrue();
+            a.Equals((object)c).Should().BeFalse();
+            a.Equals((object)null).Should().BeFalse();
+            a.CompareTo((object)a).Should().Be(0);
+            a.CompareTo((object)b).Should().Be(0);
+            a.CompareTo((object)c).Should().Be(-1);
+            a.CompareTo((object)null).Should().Be(1);
+            (a < b).Should().BeFalse();
+            (a <= b).Should().BeTrue();
+            (c > a).Should().BeTrue();
+            (c >= a).Should().BeTrue();
+            Action throws = () => a.CompareTo("");
+            throws.Should().Throw<ArgumentException>();
+        }
     }
 }
